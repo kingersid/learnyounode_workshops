d0012f7351418791e728d0ea125a8fb9b939a7d1
kingersid <kinger.siddharth@gmail.com>
2016-02-16 22:23:42 +0530

    added

---
+++ http_uppercaser.js
@@ -0,0 +1,12 @@
+var http = require('http')
+
+var map = require('through2-map')  
+
+var server = http.createServer(function(request,response){
+	if (request.method != 'POST')
+		return response.end('send me a post \n')
+	request.pipe(map(function (chunk) {  
+       	return chunk.toString().toUpperCase()  
+     })).pipe(response) 
+})
+server.listen(process.argv[2])
