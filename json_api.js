067508abddffd9be2234516835577c6e8c0b4138
kingersid <kinger.siddharth@gmail.com>
2016-02-17 19:40:47 +0530

    committed

--- json_api.js
+++ json_api.js
@@ -4,13 +4,13 @@ var http = new require('http')
 
 var server = http.createServer(function(request,response){
 	if (url.parse(request.url).pathname === '/api/parsetime' )
-		var date = new Date(url.parse(request.url,true).query.iso)
-		var response_object = {"hour":date.getHours(),"minute":date.getMinutes(),"second":date.getSeconds()}
-		response.simpleText(200, JSON.stringify(response_object))
+		dated = new Date(url.parse(request.url,true).query.iso)
+		response_object = {"hour":dated.getHours(),"minute":dated.getMinutes(),"second":dated.getSeconds()}
+		response.write(JSON.stringify(response_object))
 	if (url.parse(request.url).pathname === '/api/unixtime' )
-		var date = new Date(url.parse(request.url,true).query.iso)
-		var response_object = {"unixtime":date.getTime()}
-		response.simpleText(200, JSON.stringify(response_object))
+		dated = new Date(url.parse(request.url,true).query.iso)
+		response_object = {"unixtime":dated.getTime()}
+		response.write(JSON.stringify(response_object))
 });
 
 server.listen(process.argv[2])
